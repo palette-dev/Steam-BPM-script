1de02f16fb779ebb84ed83d2db0622a0b1ff827d
Palette <46242177+palette-dev@users.noreply.github.com>
2023-12-23 19:38:33 +0100

    Migrate tasklist call for checking steam to a function

--- Steam_BPM.bat
+++ Steam_BPM.bat
@@ -14,23 +14,24 @@ rem	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem	See the License for the specific language governing permissions and
 rem	limitations under the License.
 
-if not defined MIN set min=1 & start "" /min "%~dpnx0" %* & exit
+if not defined MIN set min=1 & start "" /min "%~f0" %* & exit
+if not "%1"=="" goto %1
 
 rem The path to your Steam executeable
 set steam_exe="C:\Program Files (x86)\Steam\steam.exe"
 rem The title of the BPM window in the language you set Steam to
 set bpm_check="Big-Picture"
 
-echo Steam BPM script v1.0 by PaletteDev
+echo Steam BPM script v1.0-dev by PaletteDev
 echo Licensed under Apache-2.0
 echo -----------------------------------
 
-tasklist /fi "IMAGENAME eq steamwebhelper.exe" /fi "USERNAME eq %USERNAME%" | find "steamwebhelper.exe" > nul || goto :steam_kill_skip
+call "%~f0" :check_steam | find "steamwebhelper.exe" > nul || goto :steam_kill_skip
 echo Closing Steam...
 start "" %STEAM_EXE% "-shutdown"
 :killed_check
 timeout /t 1 /nobreak > nul
-tasklist /fi "IMAGENAME eq steamwebhelper.exe" /fi "USERNAME eq %USERNAME%" | find "steamwebhelper.exe" > nul && goto :killed_check
+call "%~f0" :check_steam | find "steamwebhelper.exe" > nul && goto :killed_check
 :steam_kill_skip
 
 echo Starting Steam in BPM...
@@ -39,7 +40,7 @@ start "" %STEAM_EXE% "-gamepadui"
 echo Waiting for Steam BPM to start...
 :open_check
 timeout /t 5 /nobreak > nul
-tasklist /fi "IMAGENAME eq steamwebhelper.exe" /fi "USERNAME eq %USERNAME%" /V | find %BPM_CHECK% > nul || goto :open_check
+call "%~f0" :check_steam | find %BPM_CHECK% > nul || goto :open_check
 
 tasklist /fi "IMAGENAME eq explorer.exe" /fi "USERNAME eq %USERNAME%" | find "explorer.exe" > nul || goto :shell_kill_skip
 echo Killing the Windows shell...
@@ -49,10 +50,14 @@ Taskkill /f /im explorer.exe /fi "USERNAME eq %USERNAME%" > nul
 echo Running Steam BPM check...
 :closed_check
 timeout /t 10 /nobreak > nul
-tasklist /fi "IMAGENAME eq steamwebhelper.exe" /fi "USERNAME eq %USERNAME%" /V | find %BPM_CHECK% > nul && goto :closed_check
+call "%~f0" :check_steam | find %BPM_CHECK% > nul && goto :closed_check
 echo Steam BPM closed!
 
 echo Starting the Windows shell...
 start "" "%WINDIR%\explorer.exe"
 
 exit
+
+:check_steam
+tasklist /fi "IMAGENAME eq steamwebhelper.exe" /fi "USERNAME eq %USERNAME%" /V
+exit /B
